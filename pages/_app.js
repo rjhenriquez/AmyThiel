97e67508b1b3f359d1567e7dfd1412d54a259c60
Ricardo Henriquez <ricardojhenriquez@gmail.com>
2022-07-26 14:21:28 -0500

    Making the coming soon sign

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import "../styles/styles.scss";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Amy Thiel</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="The website of Amy Thiel" />
+        <link rel="home" href="https://amythiel.com" />
+        <meta name="author" content="Amy Thiel" />
+        <meta name="robots" content="index,follow" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
